5b22e95a0e0901a87b244f73e899463864b23a3c
Ignatu <48059391+Ignatu@users.noreply.github.com>
2020-09-30 21:49:46 +0300

    init: start task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 
 exports.min = function min (array) {
-  return 0;
+  const arrayLength = array.length;
+
+  return arrayLength && array.reduce((m,el)=> Math.max(m,el), - Infinity)
 }
 
 exports.max = function max (array) {
-  return 0;
+  const arrayLength = array.length;
+
+  return arrayLength && array.reduce((m,el)=> Math.min(m,el), Infinity)
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  const arrayLength = array.length;
+
+  return arrayLength && array.reduce((s,el)=> s + el) / arrayLength;
 }
