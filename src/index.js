e447a0cf83a0b29803a0cb1642f588c6acd8ac14
Ignatu <48059391+Ignatu@users.noreply.github.com>
2020-10-01 00:04:38 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 
-exports.min = array => array.length && Math.min(...array);
+exports.min = (array = []) => array.length && Math.min(...array);
 
 
-exports.max = array => array.length && Math.max(...array);
+exports.max = (array = []) => array.length && Math.max(...array);
 
 
 exports.avg = function avg (array =[] ) {
